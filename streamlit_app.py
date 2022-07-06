958dc068fd2e19238069239a4870265e7ee1435c
apol-fumio-moriya <101792563+apol-fumio-moriya@users.noreply.github.com>
2022-07-06 22:37:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,17 +15,35 @@ with st.form(key='my_form'):
     decide1 = st.radio(
         f"自分の考えに夢中になって、周囲のことを無視したり忘れることがよくある。",
         options=["Yes", "No"],
+        horizontal=True,
         )
     decide2 = st.radio(
         f"受信箱がごちゃごちゃするのが我慢できず、できるだけ早くEメールに返答しようとする。",
         options=["Yes", "No"],
+        horizontal=True,
         )
     decide3 = st.radio(
         f"プレッシャーがあるときでも常にリラックスし、集中できる。",
         options=["Yes", "No"],
+        horizontal=True,
         )
-    decide4 = st.radio(
-        f"通常、自分から話を始めることはない。",
-        options=["Yes", "No"],
-        )
+    
+    st.subheader("通常、自分から話を始めることはない。")
+    temp_options = ['はい', 'どちらでもない','いいえ']
+    temp = st.select_slider("選択してください", options=temp_options)
+    st.write("あなたが選択したものは、こちらです：", temp)
+
     submit_button = st.form_submit_button(label="次へ")
+    
+import pandas as pd
+import numpy as np
+import altair as alt
+
+df = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+
+c = alt.Chart(df).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.altair_chart(c, use_container_width=True)
